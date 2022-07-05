1f695fbec78efd038aaf45983f8ad87fafd229f1
jinhyeokgk <jinhyeok.lee@gkjeomju.com>
2022-07-05 15:10:22 +0900

    feat: user model, service 추가, module injection 관련 학습

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+const DEBUG_STATE = true;
+const PLAYGROUND_STATE = true;
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root1234',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: DEBUG_STATE,
+      playground: PLAYGROUND_STATE,
+      autoSchemaFile: 'schema.gql',
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/modules/user/index.ts
@@ -0,0 +1,4 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserAccountService } from './services/user-account.service';
+
